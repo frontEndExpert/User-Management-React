c25da1750cc53a85be56401920d7ac1659b103fd
frontEndExpert <aylon.spigel@gmail.com>
2017-09-07 22:24:42 +0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// var App = require('./components/App');
 import App from './components/App';
-// var AppAPI = require('./utils/appAPI');
 
-// AppAPI.getContacts();
 ReactDOM.render(<App />, document.getElementById('app'));
